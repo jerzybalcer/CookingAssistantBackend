73264dd99a235fc5ede5ce35d89a79aa20ca4c6a
Daniel Karnacz <56158777+danikar208@users.noreply.github.com>
2022-03-22 22:07:32 +0100

    added step constructor

--- CookingAssistant/CookingAssistant/Model/RecipeStep.cs
+++ CookingAssistant/CookingAssistant/Model/RecipeStep.cs
@@ -1,8 +1,14 @@
-﻿namespace CookingAssistant.Model
+namespace CookingAssistant.Model
 {
     public class RecipeStep
     {
         public string Instruction { get; set; }
         public TimeSpan Time { get; set; }
+        
+        RecipeStep(string instruction, TimeSpan time)
+        {
+            Instruction = instruction;
+            Time = time;
+        }
     }
 }
