16b385d1707f3fd46003b58610b56cbd53eb9cac
Daniel Karnacz <56158777+danikar208@users.noreply.github.com>
2022-03-22 22:06:15 +0100

    added ingredient constructor

--- CookingAssistant/CookingAssistant/Model/RecipeIngredient.cs
+++ CookingAssistant/CookingAssistant/Model/RecipeIngredient.cs
@@ -6,5 +6,13 @@
         public string Name { get; set; }
         public int Ammount { get; set; }
         public Units Unit { get; set; }
+        
+        public RecipeIngredient(int ingredientId, string name, int amount, Units unit)
+        {
+            IngredientId = ingredientId;
+            Name = name;
+            Ammount = amount;
+            Unit = unit;
+        }
     }
 }
