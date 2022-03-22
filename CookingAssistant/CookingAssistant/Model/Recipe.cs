2c15120c0ce87f1254cefa1455dff37e1cab27ec
Daniel Karnacz <56158777+danikar208@users.noreply.github.com>
2022-03-22 18:59:25 +0100

    added recipe constructor

--- CookingAssistant/CookingAssistant/Model/Recipe.cs
+++ CookingAssistant/CookingAssistant/Model/Recipe.cs
@@ -7,5 +7,16 @@
         public ICollection<RecipeStep> Steps{ get; set; }
         public ICollection<RecipeIngredient> Ingredients { get; set; }
         public User User { get; set; }
+
+        public Recipe(int recipeId, string name, User user)
+        {
+            RecipeId = recipeId;
+            Name = name;
+            User = user;
+
+            Steps = new List<RecipeStep>();
+            Ingredients = new List<RecipeIngredient>();
+        }
+            
     }
 }
