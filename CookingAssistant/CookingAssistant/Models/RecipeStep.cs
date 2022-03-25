e7315c4b9bca71d96dc8cb62c7e7fc8850b8a59f
Jerzy <jurekbalcer@gmail.com>
2022-03-25 19:13:23 +0100

    nullable recipe step time

--- CookingAssistant/CookingAssistant/Models/RecipeStep.cs
+++ CookingAssistant/CookingAssistant/Models/RecipeStep.cs
@@ -4,10 +4,10 @@ namespace CookingAssistant.Models
     {
         public int RecipeStepId { get; set; }
         public string Instruction { get; set; }
-        public TimeSpan Time { get; set; }
+        public TimeSpan? Time { get; set; }
         public ICollection<Comment> Comments { get; set; }
 
-        public RecipeStep(string instruction, TimeSpan time)
+        public RecipeStep(string instruction, TimeSpan? time = null)
         {
             Instruction = instruction;
             Time = time;
