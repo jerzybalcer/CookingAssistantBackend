42c75298cff5702d492804b4e4169c7cd1112bca
Jerzy <jurekbalcer@gmail.com>
2022-04-04 18:56:52 +0200

    basic controllers for recipe steps and ingredients

---
+++ CookingAssistantBackend/CookingAssistantBackend/Controllers/RecipeIngredientsController.cs
@@ -0,0 +1,109 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CookingAssistantBackend.Models;
+using CookingAssistantBackend.Models.Database;
+
+namespace CookingAssistantBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeIngredientsController : ControllerBase
+    {
+        private readonly CookingAssistantContext _context;
+
+        public RecipeIngredientsController(CookingAssistantContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/RecipeIngredients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RecipeIngredient>>> GetRecipeIngredients()
+        {
+            return await _context.RecipeIngredients.ToListAsync();
+        }
+
+        // GET: api/RecipeIngredients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RecipeIngredient>> GetRecipeIngredient(int id)
+        {
+            var recipeIngredient = await _context.RecipeIngredients.FindAsync(id);
+
+            if (recipeIngredient == null)
+            {
+                return NotFound();
+            }
+
+            return recipeIngredient;
+        }
+
+        // PUT: api/RecipeIngredients/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecipeIngredient(int id, RecipeIngredient recipeIngredient)
+        {
+            if (id != recipeIngredient.RecipeIngredientId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipeIngredient).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeIngredientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/RecipeIngredients
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<RecipeIngredient>> PostRecipeIngredient(RecipeIngredient recipeIngredient)
+        {
+            _context.RecipeIngredients.Add(recipeIngredient);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRecipeIngredient", new { id = recipeIngredient.RecipeIngredientId }, recipeIngredient);
+        }
+
+        // DELETE: api/RecipeIngredients/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecipeIngredient(int id)
+        {
+            var recipeIngredient = await _context.RecipeIngredients.FindAsync(id);
+            if (recipeIngredient == null)
+            {
+                return NotFound();
+            }
+
+            _context.RecipeIngredients.Remove(recipeIngredient);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RecipeIngredientExists(int id)
+        {
+            return _context.RecipeIngredients.Any(e => e.RecipeIngredientId == id);
+        }
+    }
+}
---
+++ CookingAssistantBackend/CookingAssistantBackend/Controllers/RecipeStepsController.cs
@@ -0,0 +1,109 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CookingAssistantBackend.Models;
+using CookingAssistantBackend.Models.Database;
+
+namespace CookingAssistantBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeStepsController : ControllerBase
+    {
+        private readonly CookingAssistantContext _context;
+
+        public RecipeStepsController(CookingAssistantContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/RecipeSteps
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RecipeStep>>> GetRecipeSteps()
+        {
+            return await _context.RecipeSteps.ToListAsync();
+        }
+
+        // GET: api/RecipeSteps/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RecipeStep>> GetRecipeStep(int id)
+        {
+            var recipeStep = await _context.RecipeSteps.FindAsync(id);
+
+            if (recipeStep == null)
+            {
+                return NotFound();
+            }
+
+            return recipeStep;
+        }
+
+        // PUT: api/RecipeSteps/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecipeStep(int id, RecipeStep recipeStep)
+        {
+            if (id != recipeStep.RecipeStepId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipeStep).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeStepExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/RecipeSteps
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<RecipeStep>> PostRecipeStep(RecipeStep recipeStep)
+        {
+            _context.RecipeSteps.Add(recipeStep);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRecipeStep", new { id = recipeStep.RecipeStepId }, recipeStep);
+        }
+
+        // DELETE: api/RecipeSteps/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecipeStep(int id)
+        {
+            var recipeStep = await _context.RecipeSteps.FindAsync(id);
+            if (recipeStep == null)
+            {
+                return NotFound();
+            }
+
+            _context.RecipeSteps.Remove(recipeStep);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RecipeStepExists(int id)
+        {
+            return _context.RecipeSteps.Any(e => e.RecipeStepId == id);
+        }
+    }
+}
