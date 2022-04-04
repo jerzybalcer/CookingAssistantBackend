1c8b4c5a9a8af04ee21cd53c7b96e56cda9dad2a
Jan Staniszewski <56159109+JanStaniszewski@users.noreply.github.com>
2022-04-04 23:35:10 +0200

    Probably full recipes controler backend

--- CookingAssistantBackend/CookingAssistantBackend/Program.cs
+++ CookingAssistantBackend/CookingAssistantBackend/Program.cs
@@ -5,7 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
