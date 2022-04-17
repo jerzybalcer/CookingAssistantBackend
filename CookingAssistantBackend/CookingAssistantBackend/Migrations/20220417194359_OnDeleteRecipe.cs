3708cae13730ef46b63785699bdfd3f32dfc5944
Jerzy <jurekbalcer@gmail.com>
2022-04-17 21:48:29 +0200

    cascade deleting steps and comments on delete recipe

---
+++ CookingAssistantBackend/CookingAssistantBackend/Migrations/20220417194359_OnDeleteRecipe.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CookingAssistantBackend.Migrations
+{
+    public partial class OnDeleteRecipe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_RecipeSteps_RecipeStepId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeSteps_Recipes_RecipeId",
+                table: "RecipeSteps");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RecipeId",
+                table: "RecipeSteps",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RecipeStepId",
+                table: "Comments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_RecipeSteps_RecipeStepId",
+                table: "Comments",
+                column: "RecipeStepId",
+                principalTable: "RecipeSteps",
+                principalColumn: "RecipeStepId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeSteps_Recipes_RecipeId",
+                table: "RecipeSteps",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "RecipeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_RecipeSteps_RecipeStepId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RecipeSteps_Recipes_RecipeId",
+                table: "RecipeSteps");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RecipeId",
+                table: "RecipeSteps",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "RecipeStepId",
+                table: "Comments",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_RecipeSteps_RecipeStepId",
+                table: "Comments",
+                column: "RecipeStepId",
+                principalTable: "RecipeSteps",
+                principalColumn: "RecipeStepId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RecipeSteps_Recipes_RecipeId",
+                table: "RecipeSteps",
+                column: "RecipeId",
+                principalTable: "Recipes",
+                principalColumn: "RecipeId");
+        }
+    }
+}
--- CookingAssistantBackend/CookingAssistantBackend/Models/Database/CookingAssistantContext.cs
+++ CookingAssistantBackend/CookingAssistantBackend/Models/Database/CookingAssistantContext.cs
@@ -28,6 +28,8 @@ namespace CookingAssistantBackend.Models.Database
             modelBuilder.Entity<Comment>().HasOne(c => c.WrittenBy).WithMany(u => u.Comments).OnDelete(DeleteBehavior.NoAction);
             modelBuilder.Entity<Like>().HasOne(l => l.Comment).WithMany(c => c.Likes).OnDelete(DeleteBehavior.NoAction);
             modelBuilder.Entity<Like>().HasOne(l => l.LikedBy).WithMany(u => u.Likes).OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<Recipe>().HasMany(r => r.Steps).WithOne().IsRequired();
+            modelBuilder.Entity<RecipeStep>().HasMany(rs => rs.Comments).WithOne().IsRequired();
         }
     }
 }
